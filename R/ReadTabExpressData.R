0b9b3e3cc94eadf2ae50fb398e3c9b4f01c44586
Matthew Oldach <moldach@ucalgary.ca>
2020-02-10 22:41:28 -0500

    Save internal functions in ReadTabExpressData() :truck:
    For testing/development purposes, the internal functions are included with user-facing function.

--- R/ReadTabExpressData.R
+++ R/ReadTabExpressData.R
@@ -57,3 +57,120 @@ ReadTabExpressData <- function(fileName) {
   dataSet <<- dataSet;
   return (1);
 }
+
+#' @title .readDataTable
+#' @description read tab delimited file
+#' @param fileName file location
+#' @return return a list (data.name, data.frame, meta.data)
+#' @details Can have many classes, stored in meta.info (starting with #)
+#' @rdname .readDataTable
+#' @export 
+.readDataTable <- function(fileName){
+  if(length(BiocGenerics::grep('\\.zip$',fileName,perl=TRUE))>0){
+    fileName <- utils::unzip(fileName);
+    if(length(fileName) > 1){
+      # test if "__MACOSX" or ".DS_Store"
+      osInx <- BiocGenerics::grep('MACOSX',fileName,perl=TRUE);
+      if(length(osInx) > 0){
+        fileName <- fileName[-osInx];
+      }
+      dsInx <- BiocGenerics::grep('DS_Store',fileName,perl=TRUE);
+      if(length(dsInx) > 0){
+        fileName <- fileName[-dsInx];
+      }
+      dat.inx <- BiocGenerics::grep(".[Tt][Xx][Tt]$", fileName);
+      if(length(dat.inx) != 1){
+        current.msg <<- "More than one text files (.txt) found in the zip file.";
+        return(0);
+      }
+    }
+  }
+  dat <- try(data.table::fread(fileName, header=TRUE, check.names=FALSE, data.table=FALSE));
+  if(class(dat) == "try-error"){
+    # try to use "tr" to remove double return characters
+    trFileName <- BiocGenerics::paste("tr -d \'\\r\' <", fileName);
+    dat <- try(data.table::fread(trFileName, header=TRUE, check.names=FALSE, data.table=FALSE));
+    if(class(dat) == "try-error"){
+      ipred::print("Using slower file reader ...");
+      formatStr <- substr(fileName, nchar(fileName)-2, nchar(fileName))
+      if(formatStr == "txt"){
+        dat <-try(readr::read.table(fileName,header=TRUE,comment.char = "", check.names=F, as.is=T));
+      }else{ # note, read.csv is more than read.table with sep=","
+        dat <-try(readr::read.csv(fileName,header=TRUE,comment.char = "", check.names=F, as.is=T));
+      }  
+    }
+  }
+  return(dat);
+}
+
+#' @title .readTabData
+#' @description read tab delimited file
+#' @param dataName file location
+#' @return return a list (data.name, data.frame, meta.data)
+#' @details read tab delimited file. Can have many classes, stored in meta.info (starting with #)
+#' @rdname .readTabData
+#' @export 
+.readTabData <- function(dataName) {
+  if(length(BiocGenerics::grep('\\.zip$',dataName,perl=TRUE))>0){
+    dataName <- utils::unzip(dataName);
+    if(length(dataName) > 1){
+      # test if "__MACOSX" or ".DS_Store"
+      osInx <- BiocGenerics::grep('MACOSX',dataName,perl=TRUE);
+      if(length(osInx) > 0){
+        dataName <- dataName[-osInx];
+      }
+      dsInx <- BiocGenerics::grep('DS_Store',dataName,perl=TRUE);
+      if(length(dsInx) > 0){
+        dataName <- dataName[-dsInx];
+      }
+      dat.inx <- BiocGenerics::grep(".[Tt][Xx][Tt]$", dataName);
+      if(length(dat.inx) != 1){
+        current.msg <<- "More than one text files (.txt) found in the zip file.";
+        return(0);
+      }
+    }
+  }
+  
+  msg <- NULL;
+  # using the powerful fread function, 10 times faster, note: default return data.table, turn off
+  dat1 <- .readDataTable(dataName);
+  
+  # look for #CLASS, could have more than 1 class labels, store in a list
+  meta.info <- list();
+  cls.inx <- BiocGenerics::grep("^#CLASS", dat1[,1]);
+  if(length(cls.inx) > 0){ 
+    for(i in 1:length(cls.inx)){
+      inx <- cls.inx[i];
+      cls.nm <- substring(dat1[inx, 1],2); # discard the first char #
+      if(nchar(cls.nm) > 6){
+        cls.nm <- substring(cls.nm, 7); # remove class
+      }
+      cls.lbls <- dat1[inx, -1];
+      # test NA
+      na.inx <- rlang::is_na(cls.lbls);
+      cls.lbls[na.inx] <- "NA";
+      cls.lbls <- ClearFactorStrings(cls.nm, cls.lbls);
+      
+      meta.info[[cls.nm]] <- cls.lbls;
+    }
+  }else{
+    current.msg <<- "No metadata labels #CLASS found in your data!";
+    return("F");
+  }
+  
+  meta.info <- data.frame(meta.info);
+  
+  # now remove all comments in dat1
+  # assign rownames after covert to matrix as data.frame does not allow duplicate names
+  comments.inx <- BiocGenerics::grep("^#", dat1[,1]);
+  dat1.nms <- dat1[-comments.inx,1];
+  dat1<-dat1[-comments.inx,-1];
+  dat1 <- data.matrix(dat1);
+  BiocGenerics::rownames(dat1) <- dat1.nms;
+  
+  list(
+    name= BiocGenerics::basename(dataName),
+    data=dat1,
+    meta.info=meta.info
+  );
+}
